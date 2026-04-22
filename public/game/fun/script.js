5646e520f0ecc7e370a911075a8d86c52d398192
myproxy0108-prog <myproxy0108@gmail.com>
2026-04-22 11:07:54 +0900

    Add files via upload

---
+++ public/game/fun/script.js
@@ -0,0 +1,10 @@
+// Creepypasta!
+
+// Based on the original 4chan thread.
+// No Images - No JavaScript - Pure HTML - Too much CSS!
+// Only HTML, CSS and the teeny-tiniest amount of JavaScript required to play sound.
+
+// WARNING! Playing on Safari may result in actual noclip!
+
+// Follow me for more: bsky.app/profile/ivorjetski.bsky.social
+// The making of: https://www.youtube.com/watch?v=xiEZ54lGFt8
